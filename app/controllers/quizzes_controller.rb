4ee8148e110c4b13877c53f1012b2f63388eefc8
takuro3000 <talangxiaotian6@gmail.com>
2026-03-29 13:38:48 +0900

    showアクションの作成

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,12 @@
 class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:show]
+
   def show
+  end
+
+  private
+
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
   end
 end
